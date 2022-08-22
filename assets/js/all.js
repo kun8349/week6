1553c7d623d1999f6a53acf110b7b94b7ebc47ab
kun8349 <108168475+kun8349@users.noreply.github.com>
2022-08-22 12:11:21 +0800

    Update 2022-08-22T04:11:18.991Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,19 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.swiper', {
+  // 分頁   
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // 左右箭頭    
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  // 滾動條
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true
+  }
 });
 //# sourceMappingURL=all.js.map
