26a4a968823cfbeaac5ef99ecd436072de1eabdf
kun8349 <108168475+kun8349@users.noreply.github.com>
2022-08-25 21:54:20 +0800

    8/25更新

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,27 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+var swiper1 = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
 });
